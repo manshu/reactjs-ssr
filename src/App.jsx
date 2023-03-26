c42fe2fb59708d5d0df35ed43dbbb6c2b20a9680
Himanshu Batra <himanshu@batra.io>
2023-03-25 22:44:08 -0400

    WiP

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Posts from './pages/Posts'
 
 
 const App = () => {
